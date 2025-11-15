187395e46dccfa6c8142ceaaf478f11a7f6c5e8d
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-11-15 11:47:20 +0700

    Hello world using actix web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world")
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Server started on http:localhost:8080");
+
+    HttpServer::new(|| {
+        App::new()
+        .service(hello) // register / endpoint
+    })
+    .bind(("127.0.0.1", 8081))?
+    .run()
+    .await
+
 }
